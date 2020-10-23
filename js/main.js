de7d5f1739f40d7bc11ee6f73f36d3107b29c45d
crisgarm <crisgarm9@gmail.com>
2020-10-23 12:47:35 +0200

    Add default photo

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 const input = document.querySelector(".js-input");
 const button = document.querySelector(".js-button");
-const container = document.querySelector(".js-container");
+const listContainer = document.querySelector(".js-list");
 let results = "";
 
 function getInfoFromApi() {
   fetch("http://api.tvmaze.com/search/shows?q=" + input.value)
     .then((response) => response.json())
     .then((data) => {
-      container.innerHTML = "";
+      listContainer.innerHTML = "";
       for (let i = 0; i < data.length; i++) {
         results = data[i].show;
         console.log(data[i].show);
@@ -22,9 +22,13 @@ function render() {
   let html = "";
   html += "<li>";
   html += `<p>${results.name}</p>`;
-  html += `<img src="${results.image.medium}">`;
+  if (results.image === null) {
+    html += `<img src="https://via.placeholder.com/210x295/ffffff/666666"/>`;
+  } else {
+    html += `<img src="${results.image.medium}">`;
+  }
   html += "</li>";
-  container.innerHTML += html;
+  listContainer.innerHTML += html;
 }
 
 button.addEventListener("click", getInfoFromApi);
